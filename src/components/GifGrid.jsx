961a77da83e38d4daee9905d5e9832397bc07bfa
Juanma De los Santos <juanmadls6@gmail.com>
2023-01-11 19:49:44 -0300

    ultimas modificaciones

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -10,6 +10,9 @@ export const GifGrid = ({ category }) => {
     <>
 
       <h3>{ category }</h3>
+      {
+        isLoading && (<h2 >Cargando ...</h2>)
+      }
       <div className="card-grid">
         { images.map( ( image ) => (
           < GifItem 
